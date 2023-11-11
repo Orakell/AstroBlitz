54adb5b9b9c47ab64bd1e89c51d652e7e68afad1
Orakel <36694523+Orakell@users.noreply.github.com>
2023-11-11 16:04:34 +0100

    player can die

--- Player.gd
+++ Player.gd
@@ -17,13 +17,15 @@ func _physics_process(delta):
 	if Input.is_action_pressed("rotate_cw"):
 		angular_velocity = -rotation_speed
 	
-	if (Input.is_action_pressed("go_forward")):
+	if Input.is_action_pressed("go_forward"):
 		apply_force(Vector2.UP.rotated(rotation) * thrust_force)
 	
 	if Input.is_action_pressed("fire"):
 		pass
 	
-	pass
-
 func get_sprite_size():
 	return sprite_size
+
+
+func _on_body_entered(body):
+	queue_free()
