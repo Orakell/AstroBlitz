4b691ec6029ab600067487ce9c56ce0b3857101e
Orakel <36694523+Orakell@users.noreply.github.com>
2023-11-11 15:17:48 +0100

    ship moves

---
+++ Player.gd
@@ -0,0 +1,29 @@
+extends RigidBody2D
+
+class_name Player
+
+var rotation_speed = TAU
+var thrust_force = 400
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every physic frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	angular_velocity = 0
+	
+	if Input.is_action_pressed("rotate_ccw"):
+		angular_velocity = rotation_speed
+	
+	if Input.is_action_pressed("rotate_cw"):
+		angular_velocity = -rotation_speed
+	
+	if (Input.is_action_pressed("go_forward")):
+		apply_force(Vector2.UP.rotated(rotation) * thrust_force)
+	
+	if Input.is_action_pressed("fire"):
+		pass
+	
+	pass
