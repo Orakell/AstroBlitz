3fea33ccb075280de36f82be0fe892dce2ed7a81
Orakel <36694523+Orakell@users.noreply.github.com>
2023-11-11 21:17:51 +0100

    score + lives + ui + reset game

--- main.gd
+++ main.gd
@@ -1,27 +1,73 @@
 extends Node
 
+signal score_changed
+signal level_changed
+signal lives_changed
+
+signal GAME_OVER
+
 var player_scene = preload("res://player.tscn")
 var asteroid_scene = preload("res://asteroid_big.tscn")
+@export var asteroid_container : Node2D
 
 var asteroid_spawn_range_min = 100
 var asteroid_spawn_range_max = 300
 
-var player_node
+var player_node : Node2D
+
+var score:
+	get:
+		return score
+	set(value):
+		score = value
+		score_changed.emit(score)
+		
+var lives:
+	get:
+		return lives
+	set(value):
+		lives = value
+		lives_changed.emit(lives)
+		
+var level:
+	get:
+		return level
+	set(value):
+		level = value
+		level_changed.emit(level)
 
 @onready var viewport_size = get_viewport().size
 
 func _ready():
+	setup_new_game()
+	
+func setup_new_game():
+	cleanup_game()
+	lives = 3
+	score = 0
+	level = 0
 	setup_new_level(3)
 
 func setup_new_level(asteroid_number):
+	level += 1
 	spawn_player()
 	
 	for i in asteroid_number:
 		spawn_asteroid()
+
+func cleanup_game():
+	if player_node:
+		player_node.queue_free()
+		player_node = null
+	
+	for asteroid in asteroid_container.get_children():
+		asteroid.queue_free()
+	
 	
 func spawn_player():
 	if player_node:
 		player_node.queue_free()
+		player_node = null
 	
 	player_node = player_scene.instantiate()
 	player_node.position = viewport_size / 2.0
@@ -31,8 +77,22 @@ func spawn_player():
 func spawn_asteroid():
 	var asteroid = asteroid_scene.instantiate()
 	asteroid.position = viewport_size / 2.0 + (Utils.random_unit_vector2() * randf_range(asteroid_spawn_range_min, asteroid_spawn_range_max))
-	add_child(asteroid)
+	asteroid_container.add_child(asteroid)
 
 func _on_player_death():
+	if lives <= 0:
+		game_over()
+		return
+		
+	lives -= 1
 	spawn_player()
 
+func add_to_score(number):
+	score += number
+
+func game_over():
+	if player_node:
+		player_node.queue_free()
+		player_node = null
+		
+	GAME_OVER.emit()
